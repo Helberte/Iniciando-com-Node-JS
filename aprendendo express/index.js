83f441baab8b956f210a0519a0eaa43f02315630
HELBERTE ARRUDA <helberteads@gmail.com>
2022-08-03 17:49:13 -0400

    Adição de parâmetros e parâmetros opcionais

--- aprendendo express/index.js
+++ aprendendo express/index.js
@@ -1,8 +1,6 @@
 const express = require('express'); // importando o express
 const app = express(); // inicializando o express dento da variavel app
 
-
-
 // criando uma rota, forma mais simples
 // a barra significa o diretorio raiz do app
 app.get("/",function(req, res){
@@ -26,21 +24,49 @@ app.get('/portfolio/helberte', (req, res) => {
   res.send('Vem vindo ao meu portfolio');
 });
 
-
-
 // o segundo parametro define o que a rota faz e recebe dois
 // parametros chamados, res = resposta e req = requisição
 
 
+// PASSAGEM DE PARÂMETROS
+
+app.get('/ola/:nome/:empresa', function(req, res){
+
+  let nome = req.params.nome;
+  let empresa = req.params.empresa;
 
 
+  res.send("<h1>Oi " + nome + " empresa: " + empresa  + "</h1>");
+});
+
+// quando coloca /:nome_qualquer está criando um parametro na
+// rota, e para trabalhar com rotas é necessário defi-las antes
+// serem usadas
+
+// eu posso ter quantos parametros forem necessários,
+// só poderei chamar aquela rota determinada, se eu passar
+// o parâmetro que foi solicitado
+
+// o objeto req que é passado automaticamente, serve para pegarmos
+// qualquer parametro que o usuário passe
+// req são os dados enviados pelo usuário
+// res é a resposta que vai ser enviada para o usuário
 
 
+// PARAMETRO NÃO OBRIGATORIO
 
+app.get('/paramNoObrigation/:artigo?',function(req, res){
 
+  let artigo = req.params.artigo;
 
+  if(artigo){
+    res.send('Artigo '+req.params.artigo);
+  }else{
+    res.send('Parametro não obrigatorio ');
+  }  
+})
 
-
+// colocando a ? no parametro, ele fica sendo opcional
 
 
 
