4a4a83712198bc5a7461c96fe52a44189257e355
HELBERTE ARRUDA <helberteads@gmail.com>
2022-08-17 18:06:09 -0400

    configurando express para arquivos estaticos

---
+++ revisando_ejs/main.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+// estou dizendo ao express que desejo usar arquivos estaticos
+// são aqueles que não são processados no back-end
+// CSS, IMAGENS, JAVASCRIPT DO FRONT-END, ARQUIVOS DE DADOS ETC
+app.use(express.static('public'));
+
+app.get('/:nome/:idade', (req, res) => {
+ 
+  var solteiro = 'SIM';
+  var nome = req.params.nome;
+  var idade = req.params.idade;
+  var exibirMSG = false;
+
+  var produtos = [
+    {nome: 'Doritos', preco: 3.14},
+    {nome: 'Cocacola', preco: 5},
+    {nome: 'leite', preco: 5}
+  ];
+
+  res.render('index', {
+    nome: nome,
+    idade: idade,
+    estadoCivil: solteiro,
+    msg: exibirMSG,
+    produtos: produtos
+  });
+});
+
+
+app.listen(8081, _ => {
+  console.log('Servidor levantado');
+});
